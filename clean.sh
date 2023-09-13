0fd16fc2109a5694c0b4204a5b9f3aa9d55e5a5e
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-09-13 11:03:12 +0530

    chore: cleanup

--- clean.sh
+++ clean.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# for identifier files
-# run
-# find -name "*Zone*" -type f -delete
 for file in *
 do
-	# if the file is an executable file
-	# and does not have .sh extension
-	if [ -x $file ] && [ ${file##*.} != "sh" ];
-	then
-		rm $file
-		echo "Deleted $file"
-	fi
+    if [ -f "$file" ] && [ "${file##*.}" = "exe" ]; then
+        rm "$file"
+        echo "Deleted $file"
+    fi
 done
--- gcc.sh
+++ gcc.sh
@@ -3,5 +3,6 @@
 # write a bash file to take one input of one file and compile it using gcc
 # and run the executable file
 
-gcc $1 -Wall -o ${1%.*} -lm
-./${1%.*}
+gcc $1 -Wall -o ${1%.*}.exe -lm
+./${1%.*}.exe
+rm ${1%.*}.exe
